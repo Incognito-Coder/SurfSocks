bfc934aa31fb775a9c264c3d6ccf41f22d5fb21d
Incognito-Coder <m2001ahmand@gmail.com>
2021-08-08 12:08:01 +0000

    Clash yaml creator function added

--- app.php
+++ app.php
@@ -2,7 +2,7 @@
 
 /**
  * Developer : Incognito Coder
- * Release : 1.0.1 2021/07/04
+ * Release : 1.4.3 2021/07/08
  * Telegram : @IC_Mods
  */
 
@@ -45,7 +45,7 @@ function Generator()
         $edited = str_replace('8585', $port, $edited);
         $new = readline("\033[33m\nSave file as ?:\033[0m ");
         if (!empty($new)) {
-            file_put_contents($new, $edited);
+            file_put_contents($new . '.json', $edited);
             file_put_contents($new . '.txt', $ssurl);
             echo "\033[32mProccess Done.\033[0m";
         } else {
@@ -56,6 +56,43 @@ function Generator()
         die($e);
     }
 }
+
+function Clasher()
+{
+    $password = readline('Now it\'s time to enter password: ');
+    $port = readline('Finally enter port number: ');
+    try {
+        echo "\nProccessing ...\n\n";
+        if (file_exists('SurfSocks.yaml')) {
+            $file = file_get_contents('SurfSocks.yaml');
+        } else {
+            die("\033[31mSurfSocks.yaml not in path\033[0m\n");
+        }
+        preg_match_all('/server: (.*?),/', $file, $servers);
+        $ips = [];
+        $domains = [];
+        foreach ($servers[1] as $host) {
+            $host2ip = gethostbyname($host);
+            array_push($ips, $host2ip);
+            array_push($domains, $host);
+            echo $host . ' => ' . $host2ip . PHP_EOL;
+        }
+        $edited = str_replace($domains, $ips, $file);
+        $edited = str_replace('PASS', $password, $edited);
+        $edited = str_replace('8585', $port, $edited);
+        $new = readline("\033[33m\nSave file as ?:\033[0m ");
+        if (!empty($new)) {
+            file_put_contents($new . '.yaml', $edited);
+            echo "\033[32mProccess Done.\033[0m";
+        } else {
+            echo 'No valid file name.';
+            exit;
+        }
+    } catch (Exception $e) {
+        die($e);
+    }
+}
+
 function Update()
 {
     $file = readline('Enter filename ends with .json: ');
@@ -79,7 +116,7 @@ function Update()
             echo $server['remarks'] . ' : ' . $server['server'] . ' >>> ' . $host2ip . PHP_EOL;
         }
         $edited = str_replace($domains, $ips, $get);
-        file_put_contents($file, $edited);
+        file_put_contents($file . '.json', $edited);
         file_put_contents($file . '.txt', $ssurl);
         echo "\033[32mProccess Done.\nFile Updated.\033[0m";
     } catch (Exception $e) {
@@ -89,7 +126,7 @@ function Update()
 
 if (isCommandLineInterface()) {
     echo "========================================================\n\tWelcome To SurfShark SS Updater/Generator\n\t\tScripter : @Incognito_Coder\n========================================================\n";
-    echo "\033[32m1 - Generate SS list with Credentials\n2 - Update SS list\n3 - About script\n4 - Exit\033[0m\n";
+    echo "\033[32m1 - Generate SS list with Credentials\n2 - Update SS list\n3 - Create Clash Config.yaml\n4 - About script\n5 - Exit\033[0m\n";
     $opt = readline("\033[36mSelect a option: \033[0m");
     switch ($opt) {
         case '1':
@@ -99,10 +136,13 @@ if (isCommandLineInterface()) {
             Update();
             break;
         case '3':
+            Clasher();
+            break;
+        case '4':
             $about_string = "Version : 1.0.1 php\nDeveloper : Alireza Ah-Mand @Incognito_Coder\nHome : https://t.me/ic_mods | https://mr-incognito.ir\nBy using this php script u can create easily shadowsocks servers in a list of surfshark vpn servers.make sure you have valid password and port.";
             echo $about_string;
             break;
-        case '4':
+        case '5':
             exit;
             break;
         default:
@@ -141,7 +181,31 @@ if (isCommandLineInterface()) {
             echo "Proccess Done.";
             echo "\nJson : <a href=\"" . FullPath() . $file_name . ".json\">Download</a>";
             echo "\nURI : <a href=\"" . FullPath() . $file_name . ".txt\">Download</a>";
-        } elseif ($_POST['method'] == 'update') {
+        } elseif ($_POST['method'] == 'clash') {
+            $password = $_POST['pass'];
+            $port = $_POST['port'];
+            $file_name = $_POST['name'];
+            echo "\nProccessing ...\n\n";
+            if (file_exists('SurfSocks.yaml')) {
+                $file = file_get_contents('SurfSocks.yaml');
+            } else {
+                die("\033[31mSurfSocks.yaml not in path\033[0m\n");
+            }
+            preg_match_all('/server: (.*?),/', $file, $servers);
+            $ips = [];
+            $domains = [];
+            foreach ($servers[1] as $host) {
+                $host2ip = gethostbyname($host);
+                array_push($ips, $host2ip);
+                array_push($domains, $host);
+                echo $host . ' => ' . $host2ip . PHP_EOL;
+            }
+            $edited = str_replace($domains, $ips, $file);
+            $edited = str_replace('PASS', $password, $edited);
+            $edited = str_replace('8585', $port, $edited);
+            file_put_contents($file_name . '.yaml', $edited);
+            echo "Proccess Done.";
+            echo "\nYaml : <a href=\"" . FullPath() . $file_name . ".yaml\">Download</a>";
         }
     }
 }
